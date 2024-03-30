dfa5e7e516d383e033291ac01892eb05b85bc4bd
Bardodev05 <andersondev0526@gmailcom>
2024-03-30 07:27:38 -0500

    agrege js

---
+++ main.js
@@ -0,0 +1,4 @@
+const carrito = document.getElementById("carrito")
+const alemento1 = document.getElementById("lista-1")
+const lista = document.querySelector("#lista-carrito tbody")
+const vaciarCarritobtn = document.getElementById("vaciar-carrito")
